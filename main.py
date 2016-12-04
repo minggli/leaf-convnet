c6fdfd38fe97d0aa6a3c63339a5cabe93111b2e1
Ming <ming.george.li@gmail.com>
2016-12-04 22:09:21 +0000

    new commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 import tensorflow as tf
 import numpy as np
 from sklearn import model_selection
 from utils import delete_folders, extract, pic_resize, batch_iter
 import warnings
 import os
+
 warnings.filterwarnings('ignore')
 
 # coding: utf-8
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
 from PIL import Image, ImageChops, ImageOps
 import os
 import shutil
 import pandas as pd
 import numpy as np
+from builtins import range
 
 
 def extract(train_data):
