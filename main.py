f7dd6708066336c711cb9e041470a4b480afb8b5
Ming <ming.george.li@gmail.com>
2016-12-09 15:42:46 +0000

    new commit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ dir_path = 'leaf/images/'
 model_path = 'models/'
 pid_label, pid_name, mapping = extract('leaf/train.csv')
 pic_names = [i.name for i in os.scandir(dir_path) if i.is_file() and i.name.endswith('.jpg')]
-input_shape = (48, 48)
+input_shape = (192, 192)
 m = input_shape[0] * input_shape[1]  # num of flat array
 n = len(set(pid_name.values()))
 
@@ -58,8 +58,8 @@ def max_pool_2x2(x):
 
 
 # First Convolution Layer
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([3, 3, 1, 64])
+b_conv1 = bias_variable([64])
 
 x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], 1])
 
@@ -67,7 +67,7 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # Second layer
-W_conv2 = weight_variable([5, 5, 32, 64])
+W_conv2 = weight_variable([3, 3, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -75,18 +75,26 @@ h_pool2 = max_pool_2x2(h_conv2)
 
 # Third layer
 
-W_conv3 = weight_variable([5, 5, 64, 64])
-b_conv3 = bias_variable([64])
+W_conv3 = weight_variable([3, 3, 64, 128])
+b_conv3 = bias_variable([128])
 
 h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
 h_pool3 = max_pool_2x2(h_conv3)
 
+# Forth layer
+
+W_conv4 = weight_variable([3, 3, 128, 128])
+b_conv4 = bias_variable([128])
+
+h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
+h_pool4 = max_pool_2x2(h_conv4)
+
 # Densely connected layer
-W_fc1 = weight_variable([6 * 6 * 64, 1024])
+W_fc1 = weight_variable([12 * 12 * 128, 1024])
 b_fc1 = bias_variable([1024])
 
-h_pool3_flat = tf.reshape(h_pool3, [-1, 6 * 6 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool3_flat, W_fc1) + b_fc1)
+h_pool4_flat = tf.reshape(h_pool4, [-1, 12 * 12 * 128])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, W_fc1) + b_fc1)
 
 # Dropout
 keep_prob = tf.placeholder(tf.float32)
@@ -152,7 +160,7 @@ delete = True
 if delete:
     delete_folders()
 
-kf_iterator = model_selection.StratifiedKFold(n_splits=10, shuffle=True)  # Stratified
+kf_iterator = model_selection.StratifiedKFold(n_splits=5, shuffle=True)  # Stratified
 
 train_x = list(pid_name.keys())  # leaf id
 train_y = list(pid_name.values())  # leaf species names
@@ -199,4 +207,3 @@ for train_index, valid_index in kf_iterator.split(train_x, train_y):
 
     if not cross_val:
         break
-
