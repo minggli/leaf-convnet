d68689406a36bd40bc979675c303b55b552b8958
Ming <ming.george.li@gmail.com>
2016-12-11 18:31:48 +0000

    new commit

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def max_pool_2x2(x):
 
 
 # First Convolution Layer
-W_conv1 = weight_variable([3, 3, d, 64])
+W_conv1 = weight_variable([5, 5, d, 64])
 b_conv1 = bias_variable([64])
 
 x_image = tf.reshape(x, [-1, input_shape[0], input_shape[1], d])
@@ -74,7 +74,7 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 # Second layer
-W_conv2 = weight_variable([3, 3, 64, 64])
+W_conv2 = weight_variable([5, 5, 64, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
@@ -98,7 +98,7 @@ b_fc2 = bias_variable([n])
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
-train_step = tf.train.GradientDescentOptimizer(learning_rate=5e-3).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=5e-4, beta1=.9, beta2=.999).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -115,6 +115,7 @@ def main(loop_num=0):
     print('\n\n\n\n starting cross validation... \n\n\n\n')
 
     for batch in batches:
+
         e = batch[0]
         i = batch[1]
         x_batch, y_batch = zip(*batch[2])
@@ -137,7 +138,7 @@ def main(loop_num=0):
 delete_folders()
 cross_val = False
 
-kf_iterator = model_selection.StratifiedKFold(n_splits=5, shuffle=True)  # Stratified
+kf_iterator = model_selection.StratifiedKFold(n_splits=5, shuffle=False)  # Stratified
 
 train_x = list(pid_name.keys())  # leaf id
 train_y = list(pid_name.values())  # leaf species names
@@ -161,7 +162,7 @@ for train_index, valid_index in kf_iterator.split(train_x, train_y):
 
     # create batches
     train = np.array(train)
-    batches = batch_iter(data=train, batch_size=200, num_epochs=5000, shuffle=True)
+    batches = batch_iter(data=train, batch_size=150, num_epochs=5000, shuffle=True)
 
     valid = np.array(valid)
     valid_x = np.array([i[0] for i in valid])
