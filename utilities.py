47aee06316a6d7807b6f0971244374b4c981ba29
Ming <ming.george.li@gmail.com>
2016-12-21 23:36:27 +0000

    new commit

--- utilities.py
+++ utilities.py
@@ -27,7 +27,7 @@ def transform(data, label, dim, input_shape, pixels=None, normalize=True):
         data = data.apply(preprocessing.scale, with_mean=False, with_std=True, axis=0)
         if pixels is not None:
             # img = pd.DataFrame(data=preprocessing.StandardScaler().fit(img).transform(img), index=img.index)
-            img = img.apply(preprocessing.scale, with_mean=False, with_std=True, axis=0)
+            img = img.apply(preprocessing.scale, with_mean=True, with_std=True, axis=0)
 
     margins = data.ix[:, data.columns.str.startswith('margin')]
     shapes = data.ix[:, data.columns.str.startswith('shape')]
