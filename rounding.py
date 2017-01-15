1339d77db77a23dabae8ed8dbffce0ee8df3a6dc
Ming <ming.george.li@gmail.com>
2017-01-15 20:21:52 +0000

    new commit

--- rounding.py
+++ rounding.py
@@ -1,9 +1,27 @@
 import numpy as np
 import pandas as pd
+from utilities import delete_folders, extract, pic_resize, batch_iter, transform, move_classified, generate_training_set
 
-data = pd.read_csv('submission.csv', encoding='utf-8', index_col='id')
 
-data = data.applymap(lambda x: np.around(x, decimals=1))
+data = pd.read_csv('0.01371.csv', encoding='utf-8', index_col='id')
 
-data.to_csv('submission.csv', encoding='utf-8', index='id')
+data_raw = data.applymap(lambda x: np.around(x, decimals=1))
 
+
+def submit(raw):
+
+    delete_folders()
+
+    move_classified(test_data=raw, train_data=data, columns=label.columns, index=test.index, path=IMAGE_PATH)
+
+    df = pd.DataFrame(data=raw, columns=label.columns, index=test.index)
+    df.to_csv('submission.csv', encoding='utf-8', header=True, index=True)
+
+MODEL_PATH = 'models/'
+IMAGE_PATH = 'leaf/images/'
+INPUT_PATH = 'leaf/'
+
+train, label, data = extract(INPUT_PATH + 'train.csv', target='species')
+_, _, test = extract(INPUT_PATH + 'test.csv')
+
+submit(data_raw)
