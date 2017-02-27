fb117e66cf36267b22b5650c6acc0263eac9a8fb
Ming <ming.george.li@gmail.com>
2017-02-27 18:14:01 +0800

    final commit

--- rounding.py
+++ rounding.py
@@ -4,6 +4,6 @@ import pandas as pd
 input_data = pd.read_csv('submission.csv', encoding='utf-8', index_col=['id'])
 
 
-output_data = input_data.applymap(lambda x: np.around(x, 4))
+output_data = input_data.applymap(lambda x: np.around(x, 2))
 
 output_data.to_csv('submission_rounded.csv', header=True, index=True, encoding='utf-8')
